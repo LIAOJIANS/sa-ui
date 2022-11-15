67e866af0755aa5274dadbc3ac549cdd710113ac
liaojs <272781702@qq.com>
2022-11-15 14:47:16 +0800

    feat: complate SaSlider

--- examples/pages/DemoSlider.tsx
+++ examples/pages/DemoSlider.tsx
@@ -1,25 +1,38 @@
 import { defineComponent, reactive } from "vue";
-import { SaSlider } from 'sa-ui'
+import { SaIcon, SaSlider } from 'sa-ui'
 
 export default defineComponent({
   setup() {
 
     const state = reactive({
       slider: 10,
-      slider1: 0,
-
+      slider1: 0
     })
     
     return () => <div>
       <SaSlider 
         v-model={ state.slider }
+        max={ 50 }
+        marks={
+          {
+            10: () => <div> <SaIcon icon="el-icon-alarm-clock" /> </div>,
+            20: () => <div style={{ fontWeight: 'bold', color: '#3C64A0' }}> 20% </div>
+          }
+        }
       />
 
-      { state.slider }
+      { state.slider } / 50
 
       <SaSlider 
         v-model={ state.slider1 }
         setp={ 10 }
+        
+        marks={
+          {
+            10: () => <div> <SaIcon icon="el-icon-alarm-clock" /> </div>,
+            20: () => <div style={{ fontWeight: 'bold', color: '#3C64A0' }}> 20% </div>
+          }
+        }
       />
     </div>
   }
--- src/packages/SaSlider/SaSlider.tsx
+++ src/packages/SaSlider/SaSlider.tsx
@@ -4,6 +4,7 @@ import './SaSlider.scss'
 import { classname, useModel, useRefs, useStyles } from "src/hooks";
 import SaTooltip from '../SaTooltip/SaTooltip'
 import { computed } from "vue";
+import { typeOf } from "js-hodgepodge";
 
 export const SaSlider = designComponent({
   name: 'SaSlider',
@@ -11,11 +12,17 @@ export const SaSlider = designComponent({
   props: {
     modelValue: { type: [String, Number], default: 0 },                   // 双向绑定值
     showTip: { type: Boolean, default: true },                            // 是否显示提示
-    setp: { type: Number }                                                // 滑动固定区间
+    setp: { type: Number },                                               // 滑动固定区间
+    range: { type: Boolean },                                             // 是否开启区间范围
+    max: { type: Number, default: 100 },                                  // 范围最大值
+    min: { type: Number, default: 0 },                                    // 范围最小值
+    disabled: { type: Boolean, default: false },                          // 是否禁用
+    marks: { type: Object }                                               // 接收自定义X提示，key是值，value是一个返回元素
   },
 
   emits: {
-    onUpdateModelValue: (val?: any) => true
+    onUpdateModelValue: (val?: any) => true,
+    onChange: (opt: { max: number, min: number, value: number }) => true
   },
 
   setup({ props, event: { emit } }) {
@@ -27,18 +34,28 @@ export const SaSlider = designComponent({
     // const tooltip = SaTooltip.use.inject(null)
 
     const innerBarStyles = useStyles(style => {
-      style.width = `${model.value}%`
+      style.width = `${Number(model.value) * 100 / props.max}%`
+      
+      if (props.disabled) {
+        style.background = '#36cfc9'
+      }
     })
 
     const buttonStyle = useStyles(style => {
-      style.left = `${model.value}%`
+      style.left = `${Number(model.value) * 100 / props.max}%`
     })
 
-    const stepMedian = computed(() => (Math.floor(100 / props.setp! / 2) ))
+    const buttonClasses = computed(() => classname([
+      'sa-slider-button',
+      {
+        ['sa-slider-button__disabled']: props.disabled
+      }
+    ]))
+
+    const stepMedian = computed(() => (Math.floor(100 / props.setp! / 2)))
 
     const handle = {
-      btnMouseLeave: () => {
-      },
+      barClick: (e: MouseEvent) => methods.drogging(e),
 
       btnMouseLeve: (e: MouseEvent) => {
 
@@ -48,7 +65,9 @@ export const SaSlider = designComponent({
       btnTouchStart: (e: MouseEvent) => {
         e.stopPropagation()
 
-        methods.addListenWindowEvent()
+        if (!props.disabled) {
+          methods.addListenWindowEvent()
+        }
       }
     }
 
@@ -57,13 +76,19 @@ export const SaSlider = designComponent({
         e.preventDefault()
 
         const pageX = e.pageX - refs.bar?.offsetLeft!
-        const barWidth = refs.bar?.offsetWidth!
+        const barWidth = (refs.bar?.offsetWidth!)
 
-        const val = Math.floor((pageX / barWidth) * 100)
+        const val = Math.floor((pageX / barWidth) * props.max)
 
-        model.value = val >= 100 ? 100 : val <= 0 ? 0 : (
-          !!props.setp ? val % props.setp > stepMedian.value ? Math.ceil(val / 10) * 10 :  Math.floor(val / 10) * 10 : val
+        model.value = val >= props.max ? props.max : val <= props.min ? props.min : (
+          !!props.setp ? val % props.setp > stepMedian.value ? Math.ceil(val / 10) * 10 : Math.floor(val / 10) * 10 : val
         )
+
+        emit.onChange({
+          max: props.max,
+          min: props.min,
+          value: model.value
+        })
       },
 
       addListenWindowEvent: () => {
@@ -80,19 +105,19 @@ export const SaSlider = designComponent({
     return {
       render: () => (
         <div class="sa-slider">
-          <div class="sa-slider-bar sa-slider--hover" ref={onRef.bar}>
+          <div class="sa-slider-bar sa-slider--hover" ref={onRef.bar} onClick={ handle.barClick }>
             <div class="sa-slider-bar--wrapper sa-slider--hover" style={innerBarStyles.value}></div>
             {
               !!props.setp &&
               new Array(props.setp)
                 .fill('')
                 .map((c, i) => {
-                  const step = Math.floor(100 / props.setp!) * i
+                  const step = Math.floor(props.max / props.setp!) * i
                   return step > model.value ? <div key={i} class='sa-slider-stop' style={{ left: `${step}%` }}></div> : null
                 })
             }
             <div
-              class="sa-slider-button"
+              class={buttonClasses.value}
               style={buttonStyle.value}
               onMousedown={handle.btnTouchStart}
               // onMouseleave={ handle.btnMouseLeave }
@@ -108,6 +133,16 @@ export const SaSlider = designComponent({
                 ) : <div class="sa-slider-button--wrapper" />
               }
             </div>
+
+            {
+              !!props.marks && Object
+                .entries(props.marks)
+                .map(([k, c]) => (
+                  <div class="sa-slider-tip" style={{ left: `${Number(k) * 100 / props.max}%` }}>
+                    { typeOf(c) === 'function' && c() }
+                  </div>
+                ))
+            }
           </div>
         </div>
       )
